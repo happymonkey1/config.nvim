a0abd185c59945a5d860429de7cf6310459e913d
happymonkey1 <7463843+happymonkey1@users.noreply.github.com>
2024-07-19 22:57:34 -0700

    feat: add lsp config

--- init.lua
+++ init.lua
@@ -5,10 +5,15 @@ vim.call('plug#begin')
 
     -- Treesitter
     Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
-    
+
     -- Fuzzy finder
     Plug 'nvim-lua/plenary.nvim'
     Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
+
+    Plug 'hrsh7th/nvim-cmp'
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    Plug 'hrsh7th/cmp-vsnip'
+    Plug 'hrsh7th/vim-vsnip'
 
     -- LSP server config
     Plug 'neovim/nvim-lspconfig'
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,12 +1,14 @@
+local vim = vim
 
 local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
+local language_servers = { 'clangd', 'rust_analyzer', 'lua_ls', 'tsserver' }
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {},
+    ensure_installed = language_servers,
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
@@ -28,3 +30,35 @@ lsp_config.ccls.setup {
 }
 
 lsp_config.ccls.setup{}
+
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
+cmp.setup({
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+    }, {
+        { name = 'buffer' }
+    }),
+    mapping = cmp.mapping.preset.insert({
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+        -- Enable super tab
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    }),
+    snippet = {
+        expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+        end
+    },
+    window = {
+        completion = cmp.config.window.bordered()
+    },
+})
+
+local caps = require('cmp_nvim_lsp').default_capabilities()
+for _, server in pairs(language_servers) do
+    lsp_config[server].setup { capabilities = caps }
+end
